0dfbe74d76c7f5a1dbff642404560bd3befaf55c
benibana2001 <benibana2001@gmail.com>
2023-08-08 22:47:44 +0900

    option page

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,28 +1,12 @@
 import { PrefectureList } from 'benibana_bookdata';
-console.log('hello');
+
 // Saves options to chrome.storage
 const saveOptions = () => {
-  // const color = document.getElementById('color')?.value;
-  // const likesColor = document.getElementById('like').checked;
-
-  // chrome.storage.sync.set(
-  //   { favoriteColor: color, likesColor: likesColor },
-  //   () => {
-  //     // Update status to let user know options were saved.
-  //     const status = document.getElementById('status');
-  //     status.textContent = 'Options saved.';
-  //     setTimeout(() => {
-  //       status.textContent = '';
-  //     }, 750);
-  //   }
-  // );
-  const elem = document.querySelector('#pref selected') as HTMLSelectElement;
-  console.log('elem', elem);
-  console.log('elem.value', elem?.value);
-  const { value, name } = elem;
+  const elem = document.getElementById('pref') as HTMLSelectElement;
+  const { value } = elem;
 
   // option保存をする
-  chrome.storage.sync.set({ value, name }, () => {
+  chrome.storage.sync.set({ value }, () => {
     // Update status to let user know options were saved.
     const status = document.getElementById('status') as HTMLDivElement;
     if (status) {
@@ -36,31 +20,34 @@ const saveOptions = () => {
   });
 };
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-const restoreOptions = () => {
-  // chrome.storage.sync.get(
-  //   { favoriteColor: 'red', likesColor: true },
-  //   (items) => {
-  //     document.getElementById('color').value = items.favoriteColor;
-  //     document.getElementById('like').checked = items.likesColor;
-  //   }
-  // );
-  const value = chrome.storage.sync.get('value');
-  const name = chrome.storage.sync.get('name');
-  console.log('value', value);
-  console.log('name', name);
-
-  const parent = document.getElementById('pref');
+const initPage = () => {
+  /**
+   * DOM生成
+   */
+  const parent = document.getElementById('pref') as HTMLSelectElement;
   PrefectureList.forEach(([id, name]) => {
     const child = document.createElement('option');
     child.value = id;
     child.text = name;
-    console.log('id', id);
-    console.log('name', name);
     parent?.append(child);
+  });
+
+  /**
+   * storageを読み込み
+   */
+  chrome.storage.sync.get('value').then(({ value }) => {
+    if (parent && value) {
+      PrefectureList.forEach((item, index) => {
+        if (item[0] === value) {
+          parent.getElementsByTagName('option')[index + 1].selected = true;
+          // TODO: breakするように なんかfor文は動作しない
+        }
+      });
+    }
   });
 };
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener('DOMContentLoaded', () => {
+  initPage();
+});
 document.getElementById('save')?.addEventListener('click', saveOptions);
